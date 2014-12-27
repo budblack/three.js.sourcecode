6c9a5f4e208ba69b9bc8c7afb4793b04d39c3ee4
Hou Chunlei <omni360@qq.com>
2014-12-27 19:58:03 +0800

    meshfacematerial
    meshfacematerial

--- src/materials/MeshDepthMaterial.js
+++ src/materials/MeshDepthMaterial.js
@@ -14,6 +14,13 @@
  * }
  */
 
+/*
+///MeshNormalMaterial方法根据参数parameters创建mesh(网格)的标准材质类型,到这里真的不知道怎么翻译才好,还不如叫NormalMaterial呢,
+///parameters参数的格式看上面.MeshNormalMaterial对象的功能函数采用,定义构造的函数原型对象来实现.大部分属性方法继承自材质的基类Material.
+*/
+///<summary>MeshDepthMaterial</summary>
+///<param name ="parameters" type="String">string类型的JSON格式材质属性参数</param>
+///<returns type="MeshDepthMaterial">返回MeshDepthMaterial,网格深度材质.</returns>
 THREE.MeshDepthMaterial = function ( parameters ) {
 
 	THREE.Material.call( this );
--- src/materials/MeshFaceMaterial.js
+++ src/materials/MeshFaceMaterial.js
@@ -1,13 +1,26 @@
 /**
  * @author mrdoob / http://mrdoob.com/
  */
-
+/*
+///MeshFaceMaterial方法根据参数materials创建mesh(网格)的复合材质类型,参数material是一个Material类型的数组对象,网格中的三角面属性materialindex定义了该三角面使用的参数material中材质对象的索引号.
+*/
+///<summary>MeshFaceMaterial</summary>
+///<param name ="materials" type="Array">Material类型的数组</param>
+///<returns type="MeshFaceMaterial">返回MeshFaceMaterial,复合材质.</returns>
 THREE.MeshFaceMaterial = function ( materials ) {
 
-	this.materials = materials instanceof Array ? materials : [];
+	this.materials = materials instanceof Array ? materials : [];	//判断参数material的类型是否是数组对象.
 
 };
+/*************************************************************
+****下面是MeshFaceMaterial对象的方法属性定义
+*************************************************************/
 
+/*clone方法
+///clone方法克隆MeshFaceMaterial对象,
+*/
+///<summary>clone</summary>
+///<returns type="MeshFaceMaterial">返回克隆的MeshFaceMaterial对象</returns>	
 THREE.MeshFaceMaterial.prototype.clone = function () {
 
 	var material = new THREE.MeshFaceMaterial();
@@ -18,6 +31,6 @@ THREE.MeshFaceMaterial.prototype.clone = function () {
 
 	}
 
-	return material;
+	return material;	//返回克隆的MeshFaceMaterial对象
 
 };
